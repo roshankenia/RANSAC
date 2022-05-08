766850f2ea3598f8ba0f6f9af6998f812947f4c7
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-05-08 00:20:53 -0400

    Direct Training

--- CleanModelTraining/ransac_clean_training.py
+++ CleanModelTraining/ransac_clean_training.py
@@ -7,6 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/1tFoS_uMOG6M3HA8fpIhSxCKc6QcIF9KK
 """
 
+from tensorflow.keras.callbacks import LearningRateScheduler
+from ResNet import ResNet20ForCIFAR10
 from tensorflow.keras import losses
 from cifar10_clean_utils import *
 from tensorflow import keras
@@ -22,22 +24,49 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
 cifar10_data = CIFAR10Data()
 trainX, trainY, testX, testY = cifar10_data.get_data(subtract_mean=True)
 
-# load pretrained model
-cleanModel = tf.keras.models.load_model(
-    '../Pre Training/cifar100_pretrain_model.h5')
-
-# model description
-# model.summary()
-
-# Compile
+weight_decay = 1e-4
 lr = 1e-1
+num_classes = 10
+cleanModel = ResNet20ForCIFAR10(input_shape=(
+    32, 32, 3), classes=num_classes, weight_decay=weight_decay)
 opt = tf.keras.optimizers.SGD(lr=lr, momentum=0.9, nesterov=False)
+cleanModel.compile(optimizer=opt,
+                   loss=losses.categorical_crossentropy,
+                   metrics=['accuracy'])
+# cleanModel.summary()
 
-cleanModel.compile(
-    optimizer=opt, loss=losses.categorical_crossentropy, metrics=['accuracy'])
+
+def lr_scheduler(epoch):
+    new_lr = lr
+    if epoch <= 21:
+        pass
+    elif epoch > 21 and epoch <= 37:
+        new_lr = lr * 0.1
+    else:
+        new_lr = lr * 0.01
+    print('new lr:%.2e' % new_lr)
+    return new_lr
+
+
+reduce_lr = LearningRateScheduler(lr_scheduler)
+
+# # load pretrained model
+# cleanModel = tf.keras.models.load_model(
+#     '../Pre Training/cifar100_pretrain_model.h5')
+
+# # model description
+# # model.summary()
+
+# # Compile
+# lr = 1e-1
+# opt = tf.keras.optimizers.SGD(lr=lr, momentum=0.9, nesterov=False)
+
+# cleanModel.compile(
+#     optimizer=opt, loss=losses.categorical_crossentropy, metrics=['accuracy'])
 
 # Fit
-r = cleanModel.fit(trainX, trainY, epochs=20)
+r = cleanModel.fit(trainX, trainY, epochs=50,
+                   batch_size=128, callbacks=[reduce_lr])
 
 # obtain results
 upperBoundAccuracy = cleanModel.evaluate(testX, testY)[1]
