9844311d16c123d3e56f8c154cdf1fe3dafedf85
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-05-10 16:53:47 -0400

    Update entropy_cross_validation.py

--- RANSAC Training/entropy_cross_validation.py
+++ RANSAC Training/entropy_cross_validation.py
@@ -96,8 +96,8 @@ cleanModel.compile(optimizer=opt,
 
 
 def lr_scheduler(epoch):
-    new_lr = 1e-1
-    if epoch <= 21:
+    new_lr = lr
+    if epoch>2 and epoch <= 21:
         new_lr = 0.1
     elif epoch > 21 and epoch <= 37:
         new_lr = lr * 0.1
